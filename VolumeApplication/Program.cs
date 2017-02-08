980045dc12004046e06cc9a6dc878be77cc3013a
K9452 <K9452@student.jamk.fi>
2017-02-08 15:32:15 +0200

    2 tehtävää

---
+++ VolumeApplication/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumeApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Volume volume = new Volume();
+
+            int luku = 0;
+            do
+            {
+
+                Console.WriteLine("Give a new volume value (1-100 ja 0 = off)" );
+                string line = Console.ReadLine();
+                luku = int.Parse(line);
+                volume.Voluumi = luku;
+                {
+                    Console.WriteLine("Amplifier volume is set to :" + volume.Voluumi);
+                }
+            }
+
+
+           while (luku != 0);
+           Console.WriteLine("Käänsit volumen 0 asentoon, sammutetaan....");
+        }
+    }
+}
---
+++ VolumeApplication/Volume.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumeApplication
+{
+    class Volume
+    {
+        private readonly int MaxVolume = 100;
+        private readonly int MinVolume = 0;
+
+        private int volume;
+
+        public int Voluumi
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if             
+                {
+                    volume = MinVolume;
+                    Console.WriteLine("Too low volume - Amplifier volume is set to minimum :" + Voluumi);
+                   
+
+                }
+                if else
+                {
+                    volume = MaxVolume;
+                    Console.WriteLine("Too much volume - Amplifier volume is set to minimum :" + Voluumi);
+                }
+               
+                
+            }
+        }
+    }
+}
